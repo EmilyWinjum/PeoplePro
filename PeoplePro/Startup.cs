e7e3a3fc490ff6826877e83502425977d82660a6
Emily Winjum <winjumc@oregonstate.edu>
2019-04-03 16:59:36 -0700

    I did a thing

--- PeoplePro/Startup.cs
+++ PeoplePro/Startup.cs
@@ -33,6 +33,8 @@ namespace PeoplePro
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<DAL.PeopleContext>(options => options.use(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
