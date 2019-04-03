bbcbe6b4a2ee2270bfd5c47c23d3a82fec488bb1
Winjum <winjumc@oregonstate.edu>
2019-04-02 18:31:07 -0700

    Add project files.

---
+++ PeoplePro/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeoplePro.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+
+        public virtual ICollection<Department> Departments { get; set; }
+
+        public virtual ICollection<Building> Buildings { get; set; }
+    }
+
+
+}
+
